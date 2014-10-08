a377efd08a42ee90105e79f9ee9a354c894a8572
Marco Syfrig <msyfrig@hsr.ch>
2014-10-08 14:12:09 +0200

    changed vote behavior: user can now upvote or downvote once. Clicking again on the upvote button when already upvoted results in removing the upvote and the same for downvotes.

--- RedditClone/src/ch/hsr/rubik/redditclone/SubmissionBean.java
+++ RedditClone/src/ch/hsr/rubik/redditclone/SubmissionBean.java
@@ -10,202 +10,198 @@ import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.AjaxBehaviorEvent;
 
-@ManagedBean(name="submissionBean")
+@ManagedBean(name = "submissionBean")
 @SessionScoped
 public class SubmissionBean implements Serializable {
-	private static final long serialVersionUID = 4096871059235510747L;
-	private String url;
-	private String title;
-	private Date submissionDate;
-	private String submittedByUser;
-	private int votes;
-	
-	private ArrayList<String> userUpvotes;
-	private ArrayList<String> userDownvotes;
-	private ArrayList<CommentBean> comments;
-	
-	@ManagedProperty(value="#{serverManager}")
-	private ServerManager manager;
-	
-	@ManagedProperty(value="#{userBean}")
-	private UserBean user;
+    private static final long serialVersionUID = 4096871059235510747L;
+    private String url;
+    private String title;
+    private Date submissionDate;
+    private String submittedByUser;
+    private int votes;
 
-	public SubmissionBean() {
-		votes = 1;
-		userUpvotes = new ArrayList<String>();
-		userDownvotes  = new ArrayList<String>();
-		comments = new ArrayList<CommentBean>();
-	}
+    private final ArrayList<String> userUpvotes;
+    private final ArrayList<String> userDownvotes;
+    private ArrayList<CommentBean> comments;
 
-	public ServerManager getManager() {
-		return manager;
-	}
+    @ManagedProperty(value = "#{serverManager}")
+    private ServerManager manager;
 
-	public void setManager(ServerManager manager) {
-		this.manager = manager;
-	}
-	
-	public UserBean getUser() {
-		return user;
-	}
+    @ManagedProperty(value = "#{userBean}")
+    private UserBean user;
 
-	public void setUser(UserBean user) {
-		this.user = user;
-	}
+    // FIXME delete me
+    public SubmissionBean() {
+        votes = 1;
+        userUpvotes = new ArrayList<String>();
+        userDownvotes = new ArrayList<String>();
+        comments = new ArrayList<CommentBean>();
+    }
 
-	/**
-	 * Creates a new instance of this class.
-	 *
-	 * @param aUrl
-	 * @param aTitle
-	 * @param aSubmissionDate
-	 * @param aSubmittedByUser
-	 */
-	public SubmissionBean(String aUrl, String aTitle, Date aSubmissionDate,
-			String aSubmittedByUser) {
-		super();
-		url = aUrl;
-		title = aTitle;
-		submissionDate = aSubmissionDate;
-		submittedByUser = aSubmittedByUser;
-		votes = 1;
-		userUpvotes = new ArrayList<String>();
-		userDownvotes  = new ArrayList<String>();
-		comments = new ArrayList<CommentBean>();
-	}
+    public SubmissionBean(final String anUrl, final String aTitle,
+            final Date aSubmissionDate, final String aSubmittedByUser) {
+        super();
+        url = anUrl;
+        title = aTitle;
+        submissionDate = aSubmissionDate;
+        submittedByUser = aSubmittedByUser;
+        votes = 1;
+        userUpvotes = new ArrayList<String>();
+        userDownvotes = new ArrayList<String>();
+        comments = new ArrayList<CommentBean>();
+    }
 
-	/**
-	 * @return the url
-	 */
-	public final String getUrl() {
-		return url;
-	}
+    public ServerManager getManager() {
+        return manager;
+    }
 
-	/**
-	 * @param aUrl the url to set
-	 */
-	public final void setUrl(String aUrl) {
-		url = aUrl;
-	}
+    public void setManager(final ServerManager manager) {
+        this.manager = manager;
+    }
 
-	/**
-	 * @return the title
-	 */
-	public final String getTitle() {
-		return title;
-	}
+    public UserBean getUser() {
+        return user;
+    }
 
-	/**
-	 * @param aTitle the title to set
-	 */
-	public final void setTitle(String aTitle) {
-		title = aTitle;
-	}
+    public void setUser(final UserBean user) {
+        this.user = user;
+    }
 
-	/**
-	 * @return the submissionDate
-	 */
-	public final Date getSubmissionDate() {
-		return submissionDate;
-	}
+    /** @return the url */
+    public final String getUrl() {
+        return url;
+    }
 
-	/**
-	 * @param aSubmissionDate the submissionDate to set
-	 */
-	public final void setSubmissionDate(Date aSubmissionDate) {
-		submissionDate = aSubmissionDate;
-	}
+    /** @param aUrl the url to set */
+    public final void setUrl(final String aUrl) {
+        url = aUrl;
+    }
 
-	/**
-	 * @return the submittedByUser
-	 */
-	public final String getSubmittedByUser() {
-		return submittedByUser;
-	}
+    /** @return the title */
+    public final String getTitle() {
+        return title;
+    }
 
-	/**
-	 * @param aSubmittedByUser the submittedByUser to set
-	 */
-	public final void setSubmittedByUser(String aSubmittedByUser) {
-		submittedByUser = aSubmittedByUser;
-	}
+    /** @param aTitle the title to set */
+    public final void setTitle(final String aTitle) {
+        title = aTitle;
+    }
 
-	/**
-	 * @return the votes
-	 */
-	public final int getVotes() {
-		return votes;
-	}
+    /** @return the submissionDate */
+    public final Date getSubmissionDate() {
+        return submissionDate;
+    }
 
-	/**
-	 * @param aVotes the votes to set
-	 */
-	public final void setVotes(int aVotes) {
-		votes = aVotes;
-	}
+    /** @param aSubmissionDate the submissionDate to set */
+    public final void setSubmissionDate(final Date aSubmissionDate) {
+        submissionDate = aSubmissionDate;
+    }
 
-	/**
-	 * @return the comments
-	 */
-	public final ArrayList<CommentBean> getComments() {
-		return comments;
-	}
+    /** @return the submittedByUser */
+    public final String getSubmittedByUser() {
+        return submittedByUser;
+    }
 
-	/**
-	 * @param aComments the comments to set
-	 */
-	public final void setComments(ArrayList<CommentBean> aComments) {
-		comments = aComments;
-	}
-	
-	public void upvote(AjaxBehaviorEvent event) {
-		//TODO: prevent votes from going over int limit, maybe even use double instead of int
-		//TODO: color arrow differently when a user has voted
-		updateUserReference();		
-		if(user!= null && user.isLoggedIn() && !userUpvotes.contains(user.getUsername())){
-			//Remove user from Downvotes because he changed to upvote
-			if(userDownvotes.contains(user.getUsername())){
-				userDownvotes.remove(user.getUsername());
-			} else {
-				userUpvotes.add(user.getUsername());
-			}
-			setVotes(votes+1);
-			//TODO: add warning that user tried to upvote more then once.
-		} else {
-			//TODO: show error message that user isn't logged in.
-			System.out.println("User tried to vote but isn't logged in");
-		}
-	}
+    /** @param aSubmittedByUser the submittedByUser to set */
+    public final void setSubmittedByUser(final String aSubmittedByUser) {
+        submittedByUser = aSubmittedByUser;
+    }
 
-	private void updateUserReference() {
-		user = (UserBean) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("userBean");
-	}
-	
-	public void downvote(AjaxBehaviorEvent event) {
-		//TODO: prevent votes from going over int limit, maybe even use double instead of int
-		//TODO: color arrow differently when a user has voted
-		updateUserReference();	
-		if(user!= null && user.isLoggedIn() && !userDownvotes.contains(user.getUsername())){  	
-			//Remove user from Upvotes because he changed to downvote
-			if(userUpvotes.contains(user.getUsername())){
-				userUpvotes.remove(user.getUsername());
-			} else {
-				userDownvotes.add(user.getUsername());
-			}
-			setVotes(votes-1);
-			//TODO: add warning that user tried to downvote more then once.
-		} else {
-			//TODO: show error message that user isn't logged in.
-			System.out.println("User tried to vote but isn't logged in");
-		}
-	}
-	
-	public void addNewSubmission(AjaxBehaviorEvent event){
-		System.out.println("submission added");
-		setSubmittedByUser(user.getUsername());
-		setSubmissionDate(new Date());
-		manager.addSubmission(this);
-		user.setShowWelcome();
-		System.out.println("all done");
-	}
+    /** @return the votes */
+    public final int getVotes() {
+        return votes;
+    }
+
+    /** @param aVotes the votes to set */
+    public final void setVotes(final int aVotes) {
+        votes = aVotes;
+    }
+
+    public final void incVotes() {
+        ++votes;
+    }
+
+    public final void decVotes() {
+        --votes;
+    }
+
+    /** @return the comments */
+    public final ArrayList<CommentBean> getComments() {
+        return comments;
+    }
+
+    /** @param aComments the comments to set */
+    public final void setComments(final ArrayList<CommentBean> aComments) {
+        comments = aComments;
+    }
+
+    private boolean userCanVote() {
+        return user != null && user.isLoggedIn();
+    }
+
+    private void updateUserReference() {
+        user = (UserBean) FacesContext.getCurrentInstance()
+                .getExternalContext().getSessionMap().get("userBean");
+    }
+
+    public void upvote(final AjaxBehaviorEvent event) {
+        // TODO: prevent votes from going over int limit, maybe even use double
+        // instead of int
+        // TODO: color arrow differently when a user has voted
+
+        updateUserReference();
+        final String username = user.getUsername();
+
+        if (userCanVote()) {
+            if (canUpvote(username)) {
+                userUpvotes.add(username);
+                incVotes();
+            } else {
+                userUpvotes.remove(username);
+                decVotes();
+            }
+        } else {
+            // TODO: show error message that user isn't logged in.
+            System.out.println("User tried to vote but isn't logged in");
+        }
+    }
+
+    public void downvote(final AjaxBehaviorEvent event) {
+        // TODO: prevent votes from going over int limit, maybe even use double
+        // instead of int
+        // TODO: color arrow differently when a user has voted
+
+        updateUserReference();
+        final String username = user.getUsername();
+
+        if (userCanVote()) {
+            if (canDownvote(username)) {
+                userDownvotes.add(username);
+                decVotes();
+            } else {
+                userDownvotes.remove(username);
+                incVotes();
+            }
+        } else {
+            // TODO: show error message that user isn't logged in.
+            System.out.println("User tried to vote but isn't logged in");
+        }
+    }
+
+    private boolean canDownvote(final String username) {
+        return !userDownvotes.contains(username);
+    }
+
+    private boolean canUpvote(final String username) {
+        return !userUpvotes.contains(username);
+    }
+
+    public void addNewSubmission(final AjaxBehaviorEvent event) {
+        System.out.println("submission added");
+        setSubmittedByUser(user.getUsername());
+        setSubmissionDate(new Date());
+        manager.addSubmission(this);
+        user.setShowWelcome();
+        System.out.println("all done");
+    }
 }
